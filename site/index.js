957f37dc3364e081834081ce498842bfc54866de
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-10-22 15:10:06 -0700

    first semi-working version

--- site/index.js
+++ site/index.js
@@ -1,5 +1,45 @@
-/* global CanvasJS */
-import data from './series.js'
+/* global Chart, $chart */
+import datasets from './series.js'
+
+const config = {
+  type: 'scatter',
+  data: {
+    datasets
+  },
+  options: {
+    scales: {
+      x: {
+        type: 'logarithmic',
+        position: 'bottom'
+      },
+      y: {
+        type: 'logarithmic',
+        position: 'left'
+      }
+    }
+  }
+}
+/*
+const config =  {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+}
+*/
+/*
 
 const logarithmic = true
 
@@ -27,3 +67,6 @@ const options = {
 const chart = new CanvasJS.Chart('$chart', options)
 
 chart.render()
+*/
+
+new Chart($chart, config)
