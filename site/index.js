b2a0b0cbab3aa15de0b5da9caa8dbd3d577fe0d5
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-10-26 22:12:17 -0700

    Fix out-of memory problem

--- site/index.js
+++ site/index.js
@@ -1,22 +1,34 @@
 /* global CanvasJS */
 import data from './series.js'
 
+const xMin = Math.min(...data.map(s => Math.min(...s.dataPoints.map(p => p.x))))
+const xMax = Math.max(...data.map(s => Math.max(...s.dataPoints.map(p => p.x))))
+const yMin = Math.min(...data.map(s => Math.min(...s.dataPoints.map(p => p.y))))
+const yMax = Math.max(...data.map(s => Math.max(...s.dataPoints.map(p => p.y))))
+
 const logarithmic = true
 
 const options = {
-  animationEnabled: true,
+  // animationEnabled: true,
   title: {
     text: 'All Objects'
   },
   axisX: {
     logarithmic,
     title: 'Radius',
-    suffix: 'm'
+    suffix: 'm',
+    minimum: xMin,
+    maximum: xMax,
+    interval: xMax / 100.0
+
   },
   axisY: {
     logarithmic,
     title: 'Mass',
-    suffix: 'kg'
+    suffix: 'kg',
+    minimum: yMin,
+    maximum: yMax,
+    interval: yMax / 100.0
   },
   legend: {
     cursor: 'pointer'
